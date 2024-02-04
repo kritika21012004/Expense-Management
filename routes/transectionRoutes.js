dc97385ba31c52e6d3e74651743b5d4aae4a5ce1
kritika21012004 <103102183+kritika21012004@users.noreply.github.com>
2024-02-05 00:24:26 +0530

    code added

---
+++ routes/transectionRoutes.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const { addTransection, getAllTransection,editTransection,deleteTransection } = require('../controllers/transectionController')
+
+
+//router object
+const router=express.Router()
+
+router.post('/add-transection',addTransection);
+router.post('/edit-transection',editTransection);
+router.post('/delete-transection',deleteTransection);
+router.post('/get-transection',getAllTransection);
+module.exports = router;
